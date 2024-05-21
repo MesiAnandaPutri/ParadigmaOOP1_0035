b08b8b4658cc6482251b13768a183e236c5a36a3
MesiAnandaPutri <mesianandaputri24@gmail.com>
2024-05-21 21:52:39 +0700

    menjalankan program utama jantung dan manusia

--- Komposisi/Komposisi.cpp
+++ Komposisi/Komposisi.cpp
@@ -1,2 +1,14 @@
 // Komposisi.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "Jantung.h"
+#include "Manusia.h"
+
+int main() {
+	manusia* varManusia = new manusia("Nanda");
+	delete varManusia;
+	return 0;
+}
