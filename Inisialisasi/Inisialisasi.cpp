f02fb98e9aac5c937df052d7f345a2870fe4ede0
MesiAnandaPutri <mesianandaputri24@gmail.com>
2024-05-21 21:45:46 +0700

    public mahasiswa

--- Inisialisasi/Inisialisasi.cpp
+++ Inisialisasi/Inisialisasi.cpp
@@ -9,3 +9,8 @@ private:
     const int id;
     string nama;
     float nilai;
+
+public:
+    mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai) {
+        //definisi
+    }
